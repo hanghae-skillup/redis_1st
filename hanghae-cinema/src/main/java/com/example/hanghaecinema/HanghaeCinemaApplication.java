04c2f39163c83bf05a2fe3a103f9fb50a35041c0
youngji <youngji.jang804@gmail.com>
2025-01-08 22:03:38 +0900

    init commit

---
+++ hanghae-cinema/src/main/java/com/example/hanghaecinema/HanghaeCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.example.hanghaecinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanghaeCinemaApplication.class, args);
+	}
+
+}
