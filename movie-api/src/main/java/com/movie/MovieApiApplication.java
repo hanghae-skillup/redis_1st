cf57e7ed93e9e22714de38dd2197aeec62775dbe
Ray Jeong <devops3199@gmail.com>
2025-01-08 22:17:26 +0900

    [refactor] change module name. api -> movie-api

--- movie-api/src/main/java/com/movie/MovieApiApplication.java
+++ movie-api/src/main/java/com/movie/MovieApiApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiApplication {
+public class MovieApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiApplication.class, args);
+        SpringApplication.run(MovieApiApplication.class, args);
     }
 }
