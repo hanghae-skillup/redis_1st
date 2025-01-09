e0f47ecfcaa5698ee22dd8362b5274a06b3fbc5f
유정현 <dbdb1114@naver.com>
2025-01-09 19:18:20 +0900

    [ fix ] 불필요 코드 제거

--- module-rds-repo/src/main/java/entity/Genre.java
+++ module-rds-repo/src/main/java/entity/Genre.java
@@ -9,10 +9,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "GENRE")
 public class Genre {
 
 	@Id
