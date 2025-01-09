26fa2df8fbc592df08ab57eca43f0fd4fbba386f
유정현 <dbdb1114@naver.com>
2025-01-09 21:47:48 +0900

    [ fix ] screen - seats 연관관계 단방향으로 변경

--- module-rds-repo/src/main/java/entity/Screen.java
+++ module-rds-repo/src/main/java/entity/Screen.java
@@ -1,14 +1,10 @@
 package entity;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
@@ -23,9 +19,6 @@ public class Screen extends BaseEntity{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "screen_id")
 	private Long screen_id;
-
-	@OneToMany(mappedBy = "screen")
-	private List<Seats> seats = new ArrayList<>();
 
 	private String name;
 }
