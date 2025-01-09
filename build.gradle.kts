0d321f32a1d4dce94edf1e617f535ee2366e8d41
kim keun tae <rmsxo2000@gmail.com>
2025-01-10 06:09:23 +0900

    프로젝트 생성 및 멀티모듈 추가(헥사고날아키텍터 구조로 패키지 나누어 생성)

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.4.1"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.hanghae"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	compileOnly("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+}
+
+subprojects {
+	apply(plugin = "java")
+	apply(plugin = "io.spring.dependency-management")
+
+	dependencies {
+		implementation("org.springframework.boot:spring-boot-starter")
+		testImplementation("org.springframework.boot:spring-boot-starter-test")
+		annotationProcessor("org.projectlombok:lombok")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
