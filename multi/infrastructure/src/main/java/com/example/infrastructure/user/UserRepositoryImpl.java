3b0f8d5d52528d69685151b5349eb1a03f3ba31b
Taehwan2 <97010824+Taehwan2@users.noreply.github.com>
2025-01-09 23:12:35 +0900

    user infra 등록

---
+++ multi/infrastructure/src/main/java/com/example/infrastructure/user/UserRepositoryImpl.java
@@ -0,0 +1,20 @@
+package com.example.infrastructure.user;
+
+import com.example.domain.user.entity.User;
+import com.example.domain.user.service.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Slf4j
+@RequiredArgsConstructor
+public class UserRepositoryImpl implements UserRepository {
+
+    private final UserJpaRepository userJpaRepository;
+
+    @Override
+    public void save(User user) {
+        userJpaRepository.save(user);
+    }
+}
