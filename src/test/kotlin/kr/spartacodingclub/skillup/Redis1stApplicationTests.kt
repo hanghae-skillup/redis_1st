6f186bde920d98e4a90b9976bde07728013fcfe3
Jaehwi Cho <jjh94210@gmail.com>
2025-01-09 21:39:24 +0900

    chore: initial project structure

---
+++ src/test/kotlin/kr/spartacodingclub/skillup/Redis1stApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.spartacodingclub.skillup
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Redis1stApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
