b75979105ddd4651fd24ecb2c7eca153bc1e95b1
kimbro97 <rlagudwog@gmail.com>
2025-01-10 12:45:38 +0900

    chore: 레이어드 아키텍쳐기반 멀티모듈 구성

--- application/src/test/java/com/example/ApplicationTest.java
+++ application/src/test/java/com/example/ApplicationTest.java
@@ -1,13 +1,11 @@
-package com.example.demo;
+package com.example;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+public class ApplicationTest {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoad() {}
 }
