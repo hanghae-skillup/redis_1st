487ef82941a22dd6663fa786d57dfc5edd8c48e4
eedys1234@naver.com <eedys1234@naver.com>
2025-01-10 20:54:20 +0900

    feat: 호출 경로 수정

--- movies/src/main/kotlin/com/redis/controller/MovieController.kt
+++ movies/src/main/kotlin/com/redis/controller/MovieController.kt
@@ -4,13 +4,15 @@ import com.redis.controller.dto.MovieResponse
 import com.redis.service.MovieService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController(value = "/movies")
+@RestController
+@RequestMapping("/v1/movies")
 class MovieController(
     private val movieService: MovieService,
 ) {
-    @GetMapping("/")
+    @GetMapping
     fun gets(): ResponseEntity<MutableList<MovieResponse>> {
         return ResponseEntity.ok(movieService.gets());
     }
