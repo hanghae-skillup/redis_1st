506c91bb92af18232a8ddbcd27d0ed9c621ba616
정소민 <yjk70702002@gmail.com>
2025-01-10 22:59:54 +0900

    fix: 등급 이름 변경

--- module-domain/src/main/java/org/example/domain/movie/AgeRating.java
+++ module-domain/src/main/java/org/example/domain/movie/AgeRating.java
@@ -1,8 +1,8 @@
 package org.example.domain.movie;
 
 public enum AgeRating {
-    ALL,
-    TWELVE_AND_UP,
-    FIFREEN_AND_UP,
-    YOUTH_NOT_ALLOWED
+    PG, // 전체관람가
+    PG_13, // 12세 이상 관람가
+    R, // 15세 이상 관람가
+    NC_17 // 청소년 관람 불가
 }
