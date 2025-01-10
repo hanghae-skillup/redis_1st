13858830bb1e3b6972acc352d3f412cc97d45f47
유정현 <dbdb1114@naver.com>
2025-01-10 21:22:31 +0900

    [ feat ] ShowingService getTodayShowing() 메소드 기능 수정
    금일 상영정보 없을 경우 분기 처리

--- module-service/src/main/java/module/service/ShowingService.java
+++ module-service/src/main/java/module/service/ShowingService.java
@@ -3,8 +3,8 @@ package module.service;
 import java.util.List;
 import java.util.Map;
 
-import dto.MovieDTO;
-import dto.ShowingDTO;
+import dto.internal.MovieDTO;
+import dto.internal.ShowingDTO;
 
 public interface ShowingService {
 	List<Map.Entry<MovieDTO, List<ShowingDTO>>> getTodayShowing();
--- module-service/src/main/java/module/service/ShowingServiceImpl.java
+++ module-service/src/main/java/module/service/ShowingServiceImpl.java
@@ -11,8 +11,8 @@ import java.util.stream.Collectors;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
-import dto.MovieDTO;
-import dto.ShowingDTO;
+import dto.internal.MovieDTO;
+import dto.internal.ShowingDTO;
 import lombok.RequiredArgsConstructor;
 import module.entity.Showing;
 import module.repository.ShowingRepository;
@@ -29,11 +29,19 @@ public class ShowingServiceImpl implements ShowingService {
 		LocalDateTime from = LocalDateTime.now().plusMinutes(30);
 		LocalDateTime to = LocalDateTime.of(LocalDate.now(), LocalTime.MAX);
 
-		return showingRepository.findShowingsByStTimeBetween(from, to).stream()
+		List<Showing> showingsByStTimeBetween = showingRepository.findShowingsByStTimeBetween(from, to);
+
+		// 금일 상영 정보가 아예 없을 경우 다음날 조회
+		if(showingsByStTimeBetween.isEmpty()){
+			LocalDateTime tomorrowFrom = LocalDateTime.of(LocalDate.now().plusDays(1L), LocalTime.MIN);
+			LocalDateTime tomorrowTo = to.plusDays(1L);
+			showingsByStTimeBetween = showingRepository.findShowingsByStTimeBetween(tomorrowFrom, tomorrowTo);
+		}
+
+		return showingsByStTimeBetween.stream()
 			.map(showingEntity -> modelMapper.map(showingEntity, ShowingDTO.class))
 			.collect(Collectors.groupingBy(ShowingDTO::getMovie))
-			.entrySet()
-			.stream()
+			.entrySet().stream()
 			.peek(entry -> entry.getValue().sort(Comparator.comparing(ShowingDTO::getStTime)))
 			.sorted(Map.Entry.comparingByKey(Comparator.comparing(MovieDTO::getOpenDay, Comparator.reverseOrder())))
 			.toList();
