c97914e0c874ea2150426e7676fee731a4c58293
정소민 <yjk70702002@gmail.com>
2025-01-10 20:34:50 +0900

    feat: Movie 엔티티 생성

---
+++ module-domain/src/main/java/org/example/domain/movie/AgeRating.java
@@ -0,0 +1,8 @@
+package org.example.domain.movie;
+
+public enum AgeRating {
+    ALL,
+    TWELVE_AND_UP,
+    FIFREEN_AND_UP,
+    YOUTH_NOT_ALLOWED
+}
---
+++ module-domain/src/main/java/org/example/domain/movie/Genre.java
@@ -0,0 +1,8 @@
+package org.example.domain.movie;
+
+public enum Genre {
+    ACTION,
+    ROMANCE,
+    HORROR,
+    SF
+}
