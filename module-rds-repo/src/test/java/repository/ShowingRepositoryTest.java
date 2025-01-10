c36d8f1b9e89bc5f9ea76258f8c7d952ef12b662
유정현 <dbdb1114@naver.com>
2025-01-10 10:35:14 +0900

    [ feat ] 상영정보 조회 메소드 작성 및 테스트

--- module-rds-repo/src/test/java/repository/ShowingRepositoryTest.java
+++ module-rds-repo/src/test/java/repository/ShowingRepositoryTest.java
@@ -1,8 +1,20 @@
 package repository;
 
+import static util.PrivateGetSet.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import entity.Showing;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -13,6 +25,24 @@ class ShowingRepositoryTest {
 	@Autowired
 	public ShowingRepositoryTest(ShowingRepository showingRepository) {
 		this.showingRepository = showingRepository;
+	}
+
+	@Test
+	@DisplayName("상영중인 영화 조회")
+	public void selectShowing() {
+		//given
+		LocalDateTime today = LocalDateTime.of(LocalDate.now(), LocalTime.MIN).plusHours(6);
+		LocalDateTime edDay = LocalDateTime.of(LocalDate.now().plusDays(2), LocalTime.MAX);
+
+		//when
+		List<Showing> showings = showingRepository.findShowingByStTimeAfterAndEdTimeBeforeOrderByOpenDay(today, edDay);
+
+		// //then
+		showings.forEach(showing -> {
+			Assertions.assertTrue(getValue(showing, "stTime", LocalDateTime.class).getDayOfMonth() <= LocalDate.now()
+				.plusDays(2L)
+				.getDayOfMonth());
+		});
 	}
 
 }
