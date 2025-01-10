99b5ef2e25b1c526e634036ef4a0069b7b930ebf
유정현 <dbdb1114@naver.com>
2025-01-10 23:31:39 +0900

    [ fix ] MovieDTO equals,hashCode 함수 추가
    getTodayShowing()메소드에서 groupBy시 필요

--- module-core/src/main/java/dto/internal/MovieDTO.java
+++ module-core/src/main/java/dto/internal/MovieDTO.java
@@ -1,6 +1,7 @@
 package dto.internal;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 import lombok.Getter;
 
@@ -14,4 +15,17 @@ public class MovieDTO {
 	private GenreDTO genre;
 	private RatingDTO rating;
 	private String plot;
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true; // 같은 객체라면 true
+		if (obj == null || getClass() != obj.getClass()) return false; // 클래스 타입 확인
+		MovieDTO mObj = (MovieDTO) obj; // 안전한 캐스팅
+		return id == mObj.id; // id 비교
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id); // id를 기준으로 hashCode 생성
+	}
 }
