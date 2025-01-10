ade946ef97112bcdecd3f3a03656ede033e73e91
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2025-01-10 21:31:57 +0900

    ✨ feat: GetMovieDetailsUseCase.execute() 구현
    가상의 MovieDetail 응답 반환

--- application/src/main/kotlin/use_case/GetMovieDetailsUseCase.kt
+++ application/src/main/kotlin/use_case/GetMovieDetailsUseCase.kt
@@ -1,10 +1,46 @@
 package use_case
 
+import aggregate.MovieDetail
+import entity.Movie
+import entity.Screen
+import entity.ShowTime
 import org.springframework.stereotype.Service
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.*
 
 @Service
 class GetMovieDetailsUseCase {
 
-    fun execute() {
+    fun execute(): MovieDetail {
+        val movie = Movie(
+            id = UUID.randomUUID(),
+            title = "범죄도시",
+            rating = "청소년 관람불가",
+            genre = "액션",
+            releaseDate = LocalDate.of(2017, 5, 17),
+            thumbnailUrl = "https://example.com/thumbnails/crime-city.jpg",
+            runtimeMinutes = 121,
+        )
+
+        val screen = Screen(
+            id = UUID.randomUUID(),
+            name = "CGV 강남점",
+            row = 5,
+            column = 5,
+        )
+
+        val showTime = ShowTime(
+            id = UUID.randomUUID(),
+            movieId = movie.id,
+            screenId = screen.id,
+            startTime = LocalDateTime.of(2025, 1, 10, 10, 0)
+        )
+
+        return MovieDetail(
+            movie = movie,
+            screens = listOf(screen),
+            showTime = listOf(showTime),
+        )
     }
 }
