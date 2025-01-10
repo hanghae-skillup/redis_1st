53200769df71bbf3bc14c3b1f8a3c4dad029af53
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2025-01-10 22:13:53 +0900

    ✨ feat: MovieResponse.fromDomain() 정의
    도메인 객체에서 DTO로 변환하는 코드 작성

--- api/src/main/kotlin/io/github/jehwanyoo/redis_1st/v1/movies/MovieResponse.kt
+++ api/src/main/kotlin/io/github/jehwanyoo/redis_1st/v1/movies/MovieResponse.kt
@@ -1,6 +1,9 @@
 package io.github.jehwanyoo.redis_1st.v1.movies
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import io.github.jehwanyoo.redis_1st.aggregate.MovieDetail
+import io.github.jehwanyoo.redis_1st.entity.Screen
+import io.github.jehwanyoo.redis_1st.entity.ShowTime
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.*
@@ -13,18 +16,60 @@ data class MovieResponse(
     @JsonProperty("release_date") val releaseDate: LocalDate,  // 개봉일
     @JsonProperty("thumbnail_url") val thumbnailUrl: String,   // 썸네일 URL
     @JsonProperty("runtime_minutes") val runtimeMinutes: Int,  // 러닝 타임(분)
-    @JsonProperty("screens") val screens: List<Screen>         // 상영관 정보 리스트
-)
+    @JsonProperty("screens") val screens: List<ScreenResponse>         // 상영관 정보 리스트
+) {
+    companion object {
+        fun fromDomain(movieDetails: List<MovieDetail>): List<MovieResponse> {
+            return movieDetails.map { movieDetail ->
+                MovieResponse(
+                    movieId = movieDetail.movie.id,
+                    title = movieDetail.movie.title,
+                    rating = movieDetail.movie.rating,
+                    genre = movieDetail.movie.genre,
+                    releaseDate = movieDetail.movie.releaseDate,
+                    thumbnailUrl = movieDetail.movie.thumbnailUrl,
+                    runtimeMinutes = movieDetail.movie.runtimeMinutes,
+                    screens = movieDetail.screens.map { screen ->
+                        ScreenResponse.fromDomain(screen, movieDetail.showTime)
+                    }
+                )
+            }
+        }
+    }
+}
 
-data class Screen(
-    @JsonProperty("screen_id") val screenId: UUID,             // 상영관 고유 식별자
-    @JsonProperty("screen_name") val screenName: String,       // 상영관 이름
-    @JsonProperty("show_times") val showTimes: List<ShowTime>, // 상영 시간 정보 리스트
-    @JsonProperty("row") val row: Int,                         // 좌석 행 수
-    @JsonProperty("column") val column: Int,                   // 좌석 열 수
-)
+data class ScreenResponse(
+    @JsonProperty("screen_id") val screenId: UUID,                      // 상영관 고유 식별자
+    @JsonProperty("screen_name") val screenName: String,                // 상영관 이름
+    @JsonProperty("show_times") val showTimes: List<ShowTimeResponse>,  // 상영 시간 정보 리스트
+    @JsonProperty("row") val row: Int,                                  // 좌석 행 수
+    @JsonProperty("column") val column: Int,                            // 좌석 열 수
+) {
+    companion object {
+        fun fromDomain(screen: Screen, showTimes: List<ShowTime>): ScreenResponse {
+            return ScreenResponse(
+                screenId = screen.id,
+                screenName = screen.name,
+                row = screen.row,
+                column = screen.column,
+                showTimes = showTimes
+                    .filter { it.screenId == screen.id }
+                    .map { ShowTimeResponse.fromDomain(it) }
+            )
+        }
+    }
+}
 
-data class ShowTime(
+data class ShowTimeResponse(
     @JsonProperty("show_time_id") val showTimeId: UUID,        // 상영 시간 고유 식별자
     @JsonProperty("start_time") val startTime: LocalDateTime   // 상영 시작 시간
-)
+) {
+    companion object {
+        fun fromDomain(showTime: ShowTime): ShowTimeResponse {
+            return ShowTimeResponse(
+                showTimeId = showTime.id,
+                startTime = showTime.startTime
+            )
+        }
+    }
+}
