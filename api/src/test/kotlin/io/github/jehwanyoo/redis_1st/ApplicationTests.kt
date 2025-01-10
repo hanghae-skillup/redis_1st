8dee44a86c8c1f0f5289e8ce547a2435b0a40ba9
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2025-01-10 20:49:04 +0900

    🎉 init: 멀티 모듈 프로젝트 초기화

---
+++ api/src/test/kotlin/io/github/jehwanyoo/redis_1st/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.jehwanyoo.redis_1st
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+rootProject.name = "redis_1st"
+
+include("api")
+include("application")
+include("domain")
+include("infrastructure")
