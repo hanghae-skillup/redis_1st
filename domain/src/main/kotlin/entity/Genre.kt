24f6004e3026912efb2ff7c45215a45d16d9c9fb
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2025-01-10 21:14:03 +0900

    ✨ feat: 기본적인 도메인 엔티티 정의
    현재 구현해야되는 기본 도메인 엔티티들 입니다.

---
+++ domain/src/main/kotlin/entity/Genre.kt
@@ -0,0 +1,8 @@
+package entity
+
+import java.util.*
+
+data class Genre(
+    val id: UUID,       // 고유 ID
+    val name: String,   // 이름
+)
---
+++ domain/src/main/kotlin/entity/Rating.kt
@@ -0,0 +1,8 @@
+package entity
+
+import java.util.*
+
+data class Rating(
+    val id: UUID,       // 고유 ID
+    val name: String,   // 이름
+)
