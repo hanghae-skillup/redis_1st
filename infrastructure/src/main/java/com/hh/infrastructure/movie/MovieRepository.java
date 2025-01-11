5d443fa7f25ceeffca075ae9fdb4ff553907aa55
kokk <kokk7310@gmail.com>
2025-01-11 16:32:47 +0900

    Infrastructure 모듈 추가 - 영화 상영 Repository 추가

---
+++ infrastructure/src/main/java/com/hh/infrastructure/movie/MovieRepository.java
@@ -0,0 +1,10 @@
+package com.hh.infrastructure.movie;
+
+import com.hh.domain.movie.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+}
---
+++ infrastructure/src/main/java/com/hh/infrastructure/movie/ScreenRepository.java
@@ -0,0 +1,9 @@
+package com.hh.infrastructure.movie;
+
+import com.hh.domain.movie.Screen;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ScreenRepository extends JpaRepository<Screen, Long> {
+}
