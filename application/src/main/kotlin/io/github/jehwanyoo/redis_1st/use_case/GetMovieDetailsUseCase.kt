8be02c6e0ef8bf1f9780ccf6249520995db2eda9
JeHwanYoo <yoojehwan1875@gmail.com>
2025-01-11 14:03:55 +0900

    ✨ feat: MovieRepository를 GetMovieDetailsUseCase에서 사용
    UseCase는 인터페이스를 참조하므로 Infra와의 의존성 감소

--- application/src/main/kotlin/io/github/jehwanyoo/redis_1st/use_case/GetMovieDetailsUseCase.kt
+++ application/src/main/kotlin/io/github/jehwanyoo/redis_1st/use_case/GetMovieDetailsUseCase.kt
@@ -1,48 +1,21 @@
 package io.github.jehwanyoo.redis_1st.use_case
 
 import io.github.jehwanyoo.redis_1st.aggregate.MovieDetail
-import io.github.jehwanyoo.redis_1st.entity.Movie
-import io.github.jehwanyoo.redis_1st.entity.Screen
-import io.github.jehwanyoo.redis_1st.entity.ShowTime
+import io.github.jehwanyoo.redis_1st.repository.MovieRepository
 import org.springframework.stereotype.Service
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.util.*
 
 @Service
-class GetMovieDetailsUseCase {
+class GetMovieDetailsUseCase(
+    private val movieRepository: MovieRepository,
+) {
 
     fun execute(): List<MovieDetail> {
-        val movie = Movie(
-            id = UUID.randomUUID(),
-            title = "범죄도시",
-            rating = "청소년 관람불가",
-            genre = "액션",
-            releaseDate = LocalDate.of(2017, 5, 17),
-            thumbnailUrl = "https://example.com/thumbnails/crime-city.jpg",
-            runtimeMinutes = 121,
-        )
-
-        val screen = Screen(
-            id = UUID.randomUUID(),
-            name = "CGV 강남점",
-            row = 5,
-            column = 5,
-        )
-
-        val showTime = ShowTime(
-            id = UUID.randomUUID(),
-            movieId = movie.id,
-            screenId = screen.id,
-            startTime = LocalDateTime.of(2025, 1, 10, 10, 0)
-        )
-
-        return listOf(
+        return movieRepository.findAll().map { movie ->
             MovieDetail(
                 movie = movie,
-                screens = listOf(screen),
-                showTime = listOf(showTime),
+                screens = emptyList(),
+                showTimes = emptyList(),
             )
-        )
+        }
     }
 }
