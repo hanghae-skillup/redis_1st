ce38e4ac22f40d45b04709986822ea3b76de4896
kimbro97 <rlagudwog@gmail.com>
2025-01-11 13:24:15 +0900

    refactor: 객체 초기화시 @RequiredArgsConstructor 사용

--- domain/src/main/java/com/example/entity/Rating.java
+++ domain/src/main/java/com/example/entity/Rating.java
@@ -1,17 +1,15 @@
 package com.example.entity;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public enum Rating {
     ALL("전체 관람가"),
     TWELVE("12세 관람가"),
     FIFTEEN("15세 관람가"),
     ADULT("청소년 관람 불가");
 
-    private String description;
-
-    Rating(String description) {
-        this.description = description;
-    }
+    private final String description;
 }
