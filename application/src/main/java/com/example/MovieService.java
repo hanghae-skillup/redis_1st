96b8aae4ae7824ea1832b662e127c87a9d9cd2dd
kimbro97 <rlagudwog@gmail.com>
2025-01-12 15:12:38 +0900

    refactor: 리스트 조회시 Sort를 사용하여 정렬작업 추가

--- application/src/main/java/com/example/MovieService.java
+++ application/src/main/java/com/example/MovieService.java
@@ -4,6 +4,7 @@ import com.example.repository.MovieRepository;
 import com.example.response.MovieResponse;
 import com.example.response.MoviesServiceResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,6 +16,6 @@ public class MovieService {
     private final MovieRepository movieRepository;
 
     public List<MovieResponse> getMovies() {
-        return MoviesServiceResponse.of(movieRepository.findMovieWithScreeningAndTheater());
+        return MoviesServiceResponse.of(movieRepository.findMovieWithScreeningAndTheater(Sort.by(Sort.Order.desc("releaseDate"))));
     }
 }
