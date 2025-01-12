087edc4bb2981a09635b6de601e6cae4e31b44a9
bmsnc <qjatjs98@gmail.com>
2025-01-12 20:08:56 +0900

    feat: adapter 모듈애서 application 의존할 수 있도록 gradle 수정

--- movie/movie-adapter/src/main/java/com/bmsnc/adapter/out/persistence/Movie.java
+++ movie/movie-adapter/src/main/java/com/bmsnc/adapter/out/persistence/Movie.java
@@ -9,8 +9,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
