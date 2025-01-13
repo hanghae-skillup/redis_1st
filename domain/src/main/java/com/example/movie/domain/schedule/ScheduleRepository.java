dfe8b19e07074ff9713d26d927135755bcf53895
나평근 <skvudrms54@gmail.com>
2025-01-13 14:30:14 +0900

    Refactor: @Repository 어노테이션 제거

--- domain/src/main/java/com/example/movie/domain/schedule/ScheduleRepository.java
+++ domain/src/main/java/com/example/movie/domain/schedule/ScheduleRepository.java
@@ -3,7 +3,6 @@ package com.example.movie.domain.schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
 }
--- domain/src/main/java/com/example/movie/domain/ticket/TicketRepository.java
+++ domain/src/main/java/com/example/movie/domain/ticket/TicketRepository.java
@@ -2,7 +2,6 @@ package com.example.movie.domain.ticket;
 
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface TicketRepository {
 
 }
