86c346cd3e0a7f4aa84664d0c79cea3f85724a1a
유정현 <dbdb1114@naver.com>
2025-01-14 19:59:02 +0900

    [ refactor ] api URI 변경
    restapi 원칙을 반영한 apiURI 설정

--- module-app/src/main/java/module/controller/ShowingController.java
+++ module-app/src/main/java/module/controller/ShowingController.java
@@ -3,30 +3,27 @@ package module.controller;
 import java.util.List;
 import java.util.Map;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import dto.external.MovieShowingResponse;
+import jakarta.annotation.Nullable;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import module.service.ShowingService;
 
-@Slf4j
-@RequiredArgsConstructor
 @RestController
-@RequestMapping(value = "/showing")
+@RequiredArgsConstructor
+@RequestMapping(value = "/api/v1/showings")
 public class ShowingController {
 
 	private final ShowingService showingService;
-	private final ModelMapper modelMapper;
 
 	@GetMapping("/all")
-	public ResponseEntity<Map> getTodayShowing() {
+	public ResponseEntity<Map> getTodayShowing(@Nullable @RequestParam String title,@Nullable @RequestParam Long genre) {
 		List<MovieShowingResponse> allShowingList = showingService.getTodayShowing();
-
 		if (allShowingList.isEmpty()) {
 			return ResponseEntity.noContent().build();
 		} else {
