0beee8ef103ca6950b471d3f765a1e6f9ae9e14a
kang yeon joo <rkdduswndi@naver.com>
2025-01-15 20:00:39 +0900

    feat: 검색 기능을 포함하도록 controller 수정 및 테스트코드 작성

--- api/src/main/java/com/example/movie/application/service/MovieService.java
+++ api/src/main/java/com/example/movie/application/service/MovieService.java
@@ -1,13 +1,13 @@
 package com.example.movie.application.service;
 
-import com.example.movie.repository.MovieRepository;
 import com.example.movie.application.convertor.DtoConvertor;
-import com.example.movie.repository.dto.MoviesNowShowingDetailDto;
 import com.example.movie.application.dto.MoviesNowShowingDetail;
+import com.example.movie.entity.movie.Genre;
+import com.example.movie.repository.MovieRepository;
+import com.example.movie.repository.dto.MoviesNowShowingDetailDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
@@ -19,7 +19,7 @@ public class MovieService {
     private final MovieRepository movieRepository;
     private final DtoConvertor dtoConvertor;
 
-    public List<MoviesNowShowingDetail> getNowShowingMovies(LocalDateTime now) {
+    public List<MoviesNowShowingDetail> getMoviesNowShowing(LocalDateTime now, Genre genre, String search) {
         List<MoviesNowShowingDetailDto> dbResults = movieRepository.findNowShowing(now);
         List<MoviesNowShowingDetail> detailsList = dtoConvertor.moviesNowScreening(dbResults);
 
