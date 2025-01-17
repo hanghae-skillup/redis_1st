f17e14407a8975e0c57c5736446ac6a3cb5fc6d0
정소민 <yjk70702002@gmail.com>
2025-01-17 21:36:29 +0900

    feat: 장르 필터 validation 추가

--- module-api/src/main/java/org/example/dto/request/MoviesFilterRequestDto.java
+++ module-api/src/main/java/org/example/dto/request/MoviesFilterRequestDto.java
@@ -1,8 +1,10 @@
 package org.example.dto.request;
 
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.lang.Nullable;
 
 @Setter
 @Getter
@@ -10,5 +12,9 @@ public class MoviesFilterRequestDto {
     @Size(max = 255)
     private String movieTitle;
 
+    @Pattern(
+            regexp = "|(?i)ACTION|ROMANCE|HORROR|SF",
+            message = "장르는 다음 중 하나여야 합니다: ACTION, ROMANCE, HORROR, SF"
+    )
     private String genre;
 }
