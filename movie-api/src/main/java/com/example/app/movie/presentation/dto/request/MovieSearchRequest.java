3d5dc75dcfcd513d711e5f2e3c0d4fcd33405f82
Ray Jeong <devops3199@gmail.com>
2025-01-18 16:03:50 +0900

    [fix] npe error from request dto

--- movie-api/src/main/java/com/example/app/movie/presentation/dto/request/MovieSearchRequest.java
+++ movie-api/src/main/java/com/example/app/movie/presentation/dto/request/MovieSearchRequest.java
@@ -5,6 +5,8 @@ import com.example.app.movie.dto.MovieSearchCommand;
 import com.example.app.movie.type.MovieGenre;
 import org.hibernate.validator.constraints.Length;
 
+import static java.util.Objects.nonNull;
+
 public record MovieSearchRequest(
         @Length(max = 255, message = "제목은 255자 이하로 검색해주세요")
         String title,
@@ -14,7 +16,7 @@ public record MovieSearchRequest(
         public static MovieSearchCommand toMovieSearchCommand(MovieSearchRequest movieSearchRequest) {
                 return MovieSearchCommand.builder()
                         .title(movieSearchRequest.title)
-                        .genre(MovieGenre.valueOf(movieSearchRequest.genre))
+                        .genre(nonNull(movieSearchRequest.genre) ? MovieGenre.valueOf(movieSearchRequest.genre) : null)
                         .build();
         }
 }
