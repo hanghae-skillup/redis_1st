ad5da4e07997613c1adc55b6e9607fb8eea37aa2
eedys1234@naver.com <eedys1234@naver.com>
2025-01-19 02:03:17 +0900

    feat: validation add

--- module-infrastructure/build/tmp/kapt3/stubs/main/com/example/redis/movie/in/MovieController.java
+++ module-infrastructure/build/tmp/kapt3/stubs/main/com/example/redis/movie/in/MovieController.java
@@ -2,7 +2,7 @@ package com.example.redis.movie.in;
 
 @org.springframework.web.bind.annotation.RestController()
 @org.springframework.web.bind.annotation.RequestMapping(value = {"/api/v1/movies"})
-@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000(\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0010!\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0002\b\u0017\u0018\u00002\u00020\u0001B\r\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J(\u0010\u0005\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\b0\u00070\u00062\b\b\u0001\u0010\t\u001a\u00020\n2\b\b\u0001\u0010\u000b\u001a\u00020\nH\u0017R\u000e\u0010\u0002\u001a\u00020\u0003X\u0092\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\f"}, d2 = {"Lcom/example/redis/movie/in/MovieController;", "", "movieUseCase", "Lcom/example/redis/movie/in/movie/MovieUseCase;", "(Lcom/example/redis/movie/in/movie/MovieUseCase;)V", "gets", "Lorg/springframework/http/ResponseEntity;", "", "Lcom/example/redis/movie/in/dto/MovieResponseDto;", "title", "", "genre", "module-infrastructure"})
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000(\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0010!\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0002\b\u0017\u0018\u00002\u00020\u0001B\r\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J,\u0010\u0005\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\b0\u00070\u00062\n\b\u0001\u0010\t\u001a\u0004\u0018\u00010\n2\n\b\u0001\u0010\u000b\u001a\u0004\u0018\u00010\nH\u0017R\u000e\u0010\u0002\u001a\u00020\u0003X\u0092\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\f"}, d2 = {"Lcom/example/redis/movie/in/MovieController;", "", "movieUseCase", "Lcom/example/redis/movie/in/movie/MovieUseCase;", "(Lcom/example/redis/movie/in/movie/MovieUseCase;)V", "gets", "Lorg/springframework/http/ResponseEntity;", "", "Lcom/example/redis/movie/in/dto/MovieResponseDto;", "title", "", "genre", "module-infrastructure"})
 public class MovieController {
     @org.jetbrains.annotations.NotNull()
     private final com.example.redis.movie.in.movie.MovieUseCase movieUseCase = null;
@@ -15,9 +15,11 @@ public class MovieController {
     @org.springframework.web.bind.annotation.GetMapping()
     @org.jetbrains.annotations.NotNull()
     public org.springframework.http.ResponseEntity<java.util.List<com.example.redis.movie.in.dto.MovieResponseDto>> gets(@org.springframework.web.bind.annotation.RequestParam(value = "title", required = false)
-    @org.jetbrains.annotations.NotNull()
+    @jakarta.validation.constraints.Size(message = "The title must be at least 1 character and no more than 197 characters long.", min = 0, max = 197)
+    @org.jetbrains.annotations.Nullable()
     java.lang.String title, @org.springframework.web.bind.annotation.RequestParam(value = "genre", required = false)
-    @org.jetbrains.annotations.NotNull()
+    @jakarta.validation.constraints.Size(message = "The title must be at least 1 character and no more than 197 characters long.", min = 0, max = 197)
+    @org.jetbrains.annotations.Nullable()
     java.lang.String genre) {
         return null;
     }
--- module-infrastructure/src/main/kotlin/com/example/redis/movie/in/MovieController.kt
+++ module-infrastructure/src/main/kotlin/com/example/redis/movie/in/MovieController.kt
@@ -2,6 +2,7 @@ package com.example.redis.movie.`in`
 
 import com.example.redis.movie.`in`.dto.MovieResponseDto
 import com.example.redis.movie.`in`.movie.MovieUseCase
+import jakarta.validation.constraints.Size
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -16,7 +17,12 @@ class MovieController(
 ) {
 
     @GetMapping
-    fun gets(@RequestParam("title", required = false) title: String, @RequestParam("genre", required = false) genre: String): ResponseEntity<MutableList<MovieResponseDto>> {
+    fun gets(
+        @RequestParam("title", required = false)
+        @Size(min = 0, max = 197, message = "The title must be at least 1 character and no more than 197 characters long.") title: String?,
+        @RequestParam("genre", required = false)
+        @Size(min = 0, max = 197, message = "The genre must be at least 1 character and no more than 197 characters long.") genre: String?
+    ): ResponseEntity<MutableList<MovieResponseDto>> {
         val movies = this.movieUseCase.gets(title, genre)
         return ResponseEntity.ok(movies.stream().map { MovieResponseDto.toDto(it) }.toList())
     }
