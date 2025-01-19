8035a3c94f7d296d8afac8ecd88a59ca7eea7589
reversalSpring <p_illusion@naver.com>
2025-01-19 21:33:33 +0900

    refactor: remove EntityGraph and use JOIN FETCH for Schedule queries

--- movie-infra/src/main/java/com/movie/infra/repository/ScheduleJpaRepository.java
+++ movie-infra/src/main/java/com/movie/infra/repository/ScheduleJpaRepository.java
@@ -2,7 +2,6 @@ package com.movie.infra.repository;
 
 import com.movie.domain.entity.Schedule;
 import com.movie.domain.repository.ScheduleRepository;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -14,6 +13,5 @@ public interface ScheduleJpaRepository extends JpaRepository<Schedule, Long>, Sc
 
     @Override
     @Query("SELECT s FROM Schedule s JOIN FETCH s.movie JOIN FETCH s.theater")
-    @EntityGraph(attributePaths = {"movie", "theater"})
     List<Schedule> findAllFetchMovieTheater();
 }
