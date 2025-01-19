63849428760af4f05594f9c6c331ee550167b04d
reversalSpring <p_illusion@naver.com>
2025-01-20 00:40:03 +0900

    feat: Implement movie search API with schedule information

--- movie-domain/src/main/java/com/movie/domain/dto/MovieProjection.java
+++
@@ -1,9 +0,0 @@
-package com.movie.domain.dto;
-
-public interface MovieProjection {
-    Long getId();
-    String getTitle();
-    String getThumbnail();
-    Integer getRunningTime();
-    String getGenre();
-} 
