bbe06e1df1a79074e6a30d4f2cb1d84d28e2547c
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2025-01-19 21:40:24 +0900

    ♻️ refactor: /api prefix 추가
    1주차 리뷰 반영

---
+++ api/src/main/kotlin/io/github/jehwanyoo/redis_1st/v1/BaseApiV1Controller.kt
@@ -0,0 +1,6 @@
+package io.github.jehwanyoo.redis_1st.v1
+
+import org.springframework.web.bind.annotation.RequestMapping
+
+@RequestMapping("/api/v1")
+abstract class BaseApiV1Controller {}
--- api/src/main/kotlin/io/github/jehwanyoo/redis_1st/v1/movies/MovieController.kt
+++ api/src/main/kotlin/io/github/jehwanyoo/redis_1st/v1/movies/MovieController.kt
@@ -1,15 +1,14 @@
 package io.github.jehwanyoo.redis_1st.v1.movies
 
+import io.github.jehwanyoo.redis_1st.v1.BaseApiV1Controller
 import io.github.jehwanyoo.redis_1st.use_case.GetMoviesUseCase
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/v1")
 class MovieController(
     private val getMovieDetailsUseCase: GetMoviesUseCase
-) {
+) : BaseApiV1Controller() {
 
     @GetMapping("/movies")
     fun getMovies(): List<MovieResponse> {
