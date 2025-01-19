eb1a04b93bffc82f762daf5cc64ed23ebbac1df0
유정현 <dbdb1114@naver.com>
2025-01-19 19:45:12 +0900

    [ feat ] service module - 캐싱 기능 구현 ( 의존성 및 설정 클래스 생성 )

---
+++ module-service/src/main/java/module/cache/managers/Caffeine.java
@@ -0,0 +1,39 @@
+package module.cache.managers;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.RequiredArgsConstructor;
+import module.cache.CacheType;
+
+@Configuration
+@EnableCaching
+@RequiredArgsConstructor
+public class Caffeine {
+
+	@Bean
+	public CaffeineCacheManager caffeineCacheManager(){
+		CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+
+		Arrays.stream(CacheType.values())
+			.forEach(cacheType -> caffeineCacheManager.registerCustomCache(
+				cacheType.getCacheName(),
+				new CaffeineCache(
+					cacheType.getCacheName(),
+					com.github.benmanes.caffeine.cache.Caffeine.newBuilder()
+						.expireAfterAccess(cacheType.getExpireAfterWriteMinutes(), TimeUnit.MINUTES)
+						.maximumSize(cacheType.getMaximumSize())
+						.build()
+				).getNativeCache()
+			));
+
+		return caffeineCacheManager;
+	}
+}
