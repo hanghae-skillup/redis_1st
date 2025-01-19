84330e7c2f78aac3fa0db66d66c337c9e4d18bf6
eedys1234@naver.com <eedys1234@naver.com>
2025-01-20 01:24:12 +0900

    feat: caffeine 라이브러리 적용하여 로컬 캐싱 기능 적요

--- module-application/src/main/kotlin/com/example/redis/movie/in/movie/MovieService.kt
+++ module-application/src/main/kotlin/com/example/redis/movie/in/movie/MovieService.kt
@@ -2,6 +2,7 @@ package com.example.redis.movie.`in`.movie
 
 import com.example.redis.movie.Movie
 import com.example.redis.movie.out.movie.MoviePort
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -10,6 +11,7 @@ class MovieService(
     private val movieAdapter: MoviePort,
 ): MovieUseCase {
 
+    @Cacheable("titleCache")
     @Transactional(readOnly = true)
     override fun gets(title: String?, genre: String?): MutableList<Movie> {
         val movies = movieAdapter.findByOrderByReleaseDateDesc(title, genre)
