2b3ca824be53480e465e9a54b03f581997395f16
reversalSpring <p_illusion@naver.com>
2025-01-19 13:56:49 +0900

    refactor: remove unnecessary HelloController

--- movie-api/src/main/java/com/movie/api/controller/MovieController.java
+++ movie-api/src/main/java/com/movie/api/controller/MovieController.java
@@ -2,6 +2,7 @@ package com.movie.api.controller;
 
 import com.movie.application.dto.MovieResponseDto;
 import com.movie.application.service.MovieService;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,7 @@ public class MovieController {
         this.movieService = movieService;
     }
 
-    @GetMapping("/api/v1/movies/now-showing")
+    @GetMapping(value = "/api/v1/movies/now-showing", produces = MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8")
     public List<MovieResponseDto> getNowShowingMovies() {
         return movieService.getNowShowingMovies();
     }
