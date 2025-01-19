71957edee1938c5a69fb0e15d3cac93efd6c5371
Taehwan2 <97010824+Taehwan2@users.noreply.github.com>
2025-01-19 23:45:31 +0900

    presentation만의 의존성 생성 및 querydsl로 인한 return 변경

--- multi/presentation/src/main/java/com/example/presentation/screening/ScreeningApiController.java
+++ multi/presentation/src/main/java/com/example/presentation/screening/ScreeningApiController.java
@@ -1,10 +1,11 @@
 package com.example.presentation.screening;
 
 import com.example.application.moviescreening.MovieScreeningFacade;
-import com.example.domain.screening.Screening;
+import com.example.domain.movies.entity.enums.Genre;
 import com.example.domain.screening.service.ScreeningService;
 import com.example.presentation.screening.model.ScreeningRequestDTO;
-import com.example.presentation.screening.model.ScreeningResponseDTO;
+import com.example.domain.screening.entity.ScreeningResponseDTO;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -25,14 +26,15 @@ public class ScreeningApiController {
         movieScreeningFacade.enrollScreen(screeningRequestDTO.getMovieId(),screeningRequestDTO.getTheaterId(),screening);
     }
 
-    @GetMapping("/")
-    public ResponseEntity<?> getAllScreeningInfo(){
-        var screeningInfos = screeningService.getAllScreeningInfo();
-        var responseDTO = screeningInfos.stream()
-                .map(ScreeningResponseDTO::of)
-                .toList();
-        log.info("responseDTO: {}", responseDTO);
-        return ResponseEntity.ok(responseDTO);
+    @GetMapping()
+    public ResponseEntity<?> getAllScreeningInfo(
+            @RequestParam(name = "movieName", required = false) @Size(max = 255) String movieName,
+            @RequestParam(name = "genre", required = false) Genre genre
+    ){
+        var screeningInfos = screeningService.getAllScreeningInfo(movieName,genre);
+
+        return ResponseEntity.ok(screeningInfos);
 
     }
 }
+
