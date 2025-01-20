ee6531cc3a9e63a94e95066a404696aef095b889
JeHwanYoo <yoojehwan1875@gmail.com>
2025-01-21 02:00:08 +0900

    📦️ package: Redis Java8 시간 직렬화를 위한 Serializer 설정

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -12,4 +12,5 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-cache")
 //    implementation("com.github.ben-manes.caffeine:caffeine")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
 }
--- infrastructure/src/main/kotlin/io/github/jehwanyoo/redis_1st/cache/CacheConfig.kt
+++ infrastructure/src/main/kotlin/io/github/jehwanyoo/redis_1st/cache/CacheConfig.kt
@@ -1,11 +1,16 @@
 package io.github.jehwanyoo.redis_1st.cache
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.cache.RedisCacheConfiguration
 import org.springframework.data.redis.cache.RedisCacheManager
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializationContext
 import java.time.Duration
 
 @Configuration
@@ -29,9 +34,18 @@ class CacheConfig {
     fun cacheManager(
         redisConnectionFactory: RedisConnectionFactory,
     ): RedisCacheManager {
+        val objectMapper = ObjectMapper()
+            .registerModule(JavaTimeModule())
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+
+        val serializer = Jackson2JsonRedisSerializer(objectMapper, Any::class.java)
+
         val defaultCacheConfig = RedisCacheConfiguration
             .defaultCacheConfig()
             .entryTtl(Duration.ofMinutes(60))
+            .serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(serializer)
+            )
 
         return RedisCacheManager.builder(redisConnectionFactory)
             .cacheDefaults(defaultCacheConfig)
