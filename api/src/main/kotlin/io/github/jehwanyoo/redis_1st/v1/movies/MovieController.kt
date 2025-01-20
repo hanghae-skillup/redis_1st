c04dcb94a26054cc2a69cc4084b066be3d17b39a
JeHwanYoo <yoojehwan1875@gmail.com>
2025-01-20 12:41:50 +0900

    ✨ feat: GET /movies/search path 구현
    검색을 위한 경로인 /movies/search path를 구현했습니다. GET /movies path를 활용할 수 있지만 이것도 OCP 룰을 지키기 위함입니다.

--- api/src/main/kotlin/io/github/jehwanyoo/redis_1st/v1/movies/MovieController.kt
+++ api/src/main/kotlin/io/github/jehwanyoo/redis_1st/v1/movies/MovieController.kt
@@ -1,18 +1,31 @@
 package io.github.jehwanyoo.redis_1st.v1.movies
 
+import io.github.jehwanyoo.redis_1st.use_case.GetMoviesByTitleContainingUseCase
 import io.github.jehwanyoo.redis_1st.v1.BaseApiV1Controller
 import io.github.jehwanyoo.redis_1st.use_case.GetMoviesUseCase
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class MovieController(
-    private val getMovieDetailsUseCase: GetMoviesUseCase
+    private val getMovieDetailsUseCase: GetMoviesUseCase,
+    private val getMoviesByTitleContainingUseCase: GetMoviesByTitleContainingUseCase,
 ) : BaseApiV1Controller() {
 
     @GetMapping("/movies")
     fun getMovies(): List<MovieResponse> {
         val movieDetail = getMovieDetailsUseCase.execute()
         return MovieResponse.fromDomain(movieDetail)
+    }
+
+    @GetMapping("/movies/search")
+    fun getMoviesSearch(@RequestParam(required = false) title: String?): List<MovieResponse> {
+        if (title != null) {
+            val movies = getMoviesByTitleContainingUseCase.execute(title)
+            return MovieResponse.fromDomain(movies)
+        } else {
+            return getMovies()
+        }
     }
 }
