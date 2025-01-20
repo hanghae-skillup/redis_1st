0d3ad1ef4520b1f91d911e1a203743d21430afdb
kokk <kokk7310@gmail.com>
2025-01-20 16:57:23 +0900

    도커로 설치한 k6 활용하여 부하 테스트 진행 -localhost가 아닌 host.docker.internal로 사용하였습니다. 이유는 k6 가 도커 컨테이너 내부에서 실행하여서 입니다. http://host.docker.internal:8080/api/v1/movie

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function () {
-  http.get('http://host.docker.internal:8080/api/v1/movie');
+  http.get('http://host.docker.internal:8080/api/v1/movie?page=0&size=10&title=e');
   sleep(1);
 }
