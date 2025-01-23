b99086837c403e49aa18a66be41aaf2479e8c670
kang yeon joo <rkdduswndi@naver.com>
2025-01-23 23:45:17 +0900

    Revert "feat: 예약 API Controller 구현"
    This reverts commit b326a56a9122412560da80a6aff58413570f94a9.

--- api/src/main/java/com/example/reservation/application/dto/ReserveResponse.java
+++
@@ -1,4 +0,0 @@
-package com.example.reservation.application.dto;
-
-public class ReserveResponse {
-}
--- domain/src/main/java/com/example/reservation/dao/ReservationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.reservation.dao;
-
-import com.example.reservation.entity.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservationRepository extends JpaRepository<Reservation,Long> {
-}
