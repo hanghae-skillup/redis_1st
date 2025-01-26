17076eeb1237df5b44554ac3211014567db7195d
Taehwan2 <97010824+Taehwan2@users.noreply.github.com>
2025-01-26 23:37:01 +0900

    비관적 lock 추가

--- multi/infrastructure/src/main/java/com/example/infrastructure/seatreservation/SeatReservationJpaRepository.java
+++ multi/infrastructure/src/main/java/com/example/infrastructure/seatreservation/SeatReservationJpaRepository.java
@@ -1,7 +1,17 @@
 package com.example.infrastructure.seatreservation;
 
-import com.example.domain.SeatReservation.SeatReservation;
+import com.example.domain.seatReservation.entity.SeatReservation;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SeatReservationJpaRepository extends JpaRepository<SeatReservation,Long> {
+
+    List<SeatReservation> findByUserId(Long userId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    List<SeatReservation> findSeatReservationBySeatNumberIn(List<String> seats);
 }
