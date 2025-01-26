2468321de7e739b1aa4cf3aea98828d229653cde
유정현 <dbdb1114@naver.com>
2025-01-27 07:13:53 +0900

    [ feat ] app module 예외처리 어드바이스 작성

--- module-app/src/main/java/module/exception/ControllerAdvice.java
+++ module-app/src/main/java/module/exception/ControllerAdvice.java
@@ -7,6 +7,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
 
+import exception.showing.ShowingNotFoundException;
+import exception.ticket.InvalidAgeForMovieException;
+import exception.ticket.InvalidSeatConditionException;
+import exception.ticket.InvalidTicketException;
+import exception.ticket.NotOnSaleTicketException;
+import exception.ticket.TooManyReservationException;
+import exception.user.UserNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -15,11 +22,57 @@ public class ControllerAdvice {
 
 	@ExceptionHandler(HandlerMethodValidationException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	protected ResponseEntity<ErrorResponse> notValidParameter(HandlerMethodValidationException e){
+	protected ResponseEntity<String> notValidParameter(HandlerMethodValidationException e) {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-			.body(ErrorResponse.builder()
-				.errorCode(HttpStatus.BAD_REQUEST.value())
-				.message(e.getParameterValidationResults().get(0).getResolvableErrors().get(0).getDefaultMessage())
-				.build());
+			.body(e.getParameterValidationResults().get(0).getResolvableErrors().get(0).getDefaultMessage());
+	}
+
+	@ExceptionHandler(UserNotFoundException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	protected ResponseEntity<String> userNotFound(UserNotFoundException e) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+			.body(e.getMessage());
+	}
+
+	@ExceptionHandler(InvalidAgeForMovieException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	protected ResponseEntity<String> invalidReservation(InvalidAgeForMovieException e) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+			.body(e.getMessage());
+	}
+
+	@ExceptionHandler(InvalidSeatConditionException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	protected ResponseEntity<String> invalidReservation(InvalidSeatConditionException e) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+			.body(e.getMessage());
+	}
+
+	@ExceptionHandler(InvalidTicketException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	protected ResponseEntity<String> invalidReservation(InvalidTicketException e) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+			.body(e.getMessage());
+	}
+
+	@ExceptionHandler(TooManyReservationException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	protected ResponseEntity<String> invalidReservation(TooManyReservationException e) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+			.body(e.getMessage());
+	}
+
+	@ExceptionHandler(NotOnSaleTicketException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	protected ResponseEntity<String> invalidReservation(NotOnSaleTicketException e) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+			.body(e.getMessage());
+	}
+
+	@ExceptionHandler(ShowingNotFoundException.class)
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	protected ResponseEntity<String> showingNotFoundException(ShowingNotFoundException e){
+		return ResponseEntity.status(HttpStatus.NO_CONTENT)
+			.body(e.getMessage());
 	}
 }
