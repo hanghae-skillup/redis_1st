77c5b05909b883a2fe5ce7fc0278f0d6338a2fd2
정소민 <yjk70702002@gmail.com>
2025-01-27 02:50:17 +0900

    fix: ScreenSchedule 테이블에 단일 인덱스 추가

--- module-domain/src/main/java/org/example/domain/screenSchedule/ScreenSchedule.java
+++ module-domain/src/main/java/org/example/domain/screenSchedule/ScreenSchedule.java
@@ -9,6 +9,10 @@ import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "ScreenSchedule", indexes = {
+        @Index(name = "movieId_idx", columnList = "movieId"),
+        @Index(name = "screenRoomId_idx", columnList = "screenRoomId"),
+})
 public class ScreenSchedule extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
