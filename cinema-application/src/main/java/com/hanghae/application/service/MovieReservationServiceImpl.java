085d34ba89cda11dad568541ae40c3dd0bf5f3a4
kim keun tae <rmsxo2000@gmail.com>
2025-01-26 11:34:48 +0900

    영화예매 API 수정 (검증 로직 추가)

--- cinema-application/src/main/java/com/hanghae/application/service/MovieReservationServiceImpl.java
+++ cinema-application/src/main/java/com/hanghae/application/service/MovieReservationServiceImpl.java
@@ -4,13 +4,16 @@ import com.hanghae.application.dto.ApiResponse;
 import com.hanghae.application.dto.MovieReservationRequestDto;
 import com.hanghae.application.port.in.MovieReservationService;
 import com.hanghae.application.port.out.MemberRepositoryPort;
+import com.hanghae.application.port.out.ScreenSeatLayoutRepositoryPort;
 import com.hanghae.application.port.out.ScreeningScheduleRepositoryPort;
 import com.hanghae.application.port.out.TicketReservationRepositoryPort;
 import com.hanghae.domain.model.Member;
+import com.hanghae.domain.model.ScreenSeatLayout;
 import com.hanghae.domain.model.ScreeningSchedule;
 import com.hanghae.domain.model.TicketReservation;
 import com.hanghae.domain.model.enums.ScreenSeat;
 import com.hanghae.domain.service.ReservationService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -21,30 +24,43 @@ import java.util.List;
 public class MovieReservationServiceImpl implements MovieReservationService {
     private final TicketReservationRepositoryPort ticketReservationRepositoryPort;
     private final ScreeningScheduleRepositoryPort screeningScheduleRepositoryPort;
+    private final ScreenSeatLayoutRepositoryPort screenSeatLayoutRepositoryPort;
     private final MemberRepositoryPort memberRepositoryPort;
     private final ReservationService reservationService;
 
     @Override
+    @Transactional
     public ApiResponse<Void> saveMovieReservation(MovieReservationRequestDto requestDto) {
-        //영화 스케줄 가져오기
-        ScreeningSchedule screeningSchedule = screeningScheduleRepositoryPort.findById(requestDto.scheduleId());
-        Member member = memberRepositoryPort.findById(requestDto.memberId());
+        Long scheduleId = requestDto.scheduleId();
+        Long memberId = requestDto.memberId();
+        int seatCount = requestDto.seatCount();
+        ScreenSeat screenSeat = requestDto.screenSeat();
 
-        // TODO :: 해당 상영관 좌석 최대값 가져오기
+        // 영화 스케줄 가져오기
+        ScreeningSchedule screeningSchedule = screeningScheduleRepositoryPort.findById(scheduleId);
+        // 회원정보 조회
+        Member member = memberRepositoryPort.findById(memberId);
+        // 선택 상영관 좌석 최대값 가져오기
+        ScreenSeatLayout screenSeatLayout = screenSeatLayoutRepositoryPort.findBySeatRowAndScreenId(screenSeat.getSeatRow(), screeningSchedule.getScreen().getScreenId());
 
-        // TODO :: 현재 예매한 좌석 수 확인하기
+        // 해당 회원의 예매내역 조회
+        int reservedTicketCount = ticketReservationRepositoryPort.countByScreeningScheduleIdAndMemberId(scheduleId, memberId);
+        // 예매 내역 5개 초과 검증
+        reservationService.validateReservationSeatLimit(reservedTicketCount, seatCount);
 
-        // TODO :: 1인당 5개로 예매 제한하기
+        // 예매할 좌석 목록 가져오기
+        List<ScreenSeat> selectedSeats = ScreenSeat.getSelectedConnectedSeats(screenSeat, seatCount);
+        // 예매하려는 좌석에 대한 예매내역 조회
+        int duplicateReservationCount = ticketReservationRepositoryPort.countByScheduleIdAndScreenSeats(scheduleId, selectedSeats);
+        // 예매 좌석 중복 내역 확인하기
+        reservationService.validateSeatAvailability(duplicateReservationCount);
 
-        // 선탹 좌석기준으로 예약정보 가져오기
-        List<TicketReservation> ticketReservations =
-                reservationService.generateTicketReservationsBySeat(
-                        screeningSchedule, member, requestDto.screenSeat(), requestDto.seatCount());
+        // 예매 내역 생성
+        List<TicketReservation> ticketReservations = reservationService.createTicketReservations(screeningSchedule, member, screenSeat, screenSeatLayout, seatCount);
 
-        // TODO :: 중복 예매 내역 확인하기
+        // 예매 내역 저장
+        ticketReservationRepositoryPort.saveMovieReservations(ticketReservations);
 
-        //좌석 저장
-        List<TicketReservation> savedTicketReservations = ticketReservationRepositoryPort.saveMovieReservations(ticketReservations);
-        return ApiResponse.of("Success", 201);
+        return ApiResponse.of("예매가 완료 되었습니다.", 201);
     }
 }
--- cinema-domain/src/main/java/com/hanghae/domain/service/ReservationService.java
+++ cinema-domain/src/main/java/com/hanghae/domain/service/ReservationService.java
@@ -1,29 +1,54 @@
 package com.hanghae.domain.service;
 
 import com.hanghae.domain.model.Member;
+import com.hanghae.domain.model.ScreenSeatLayout;
 import com.hanghae.domain.model.ScreeningSchedule;
 import com.hanghae.domain.model.TicketReservation;
 import com.hanghae.domain.model.enums.ScreenSeat;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class ReservationService {
-    public List<TicketReservation> generateTicketReservationsBySeat(ScreeningSchedule screeningSchedule, Member member, ScreenSeat screenSeat, int seatCount) {
-        List<ScreenSeat> selectedSeats = ScreenSeat.getSelectedConnectedSeats(screenSeat, seatCount);
+    /* 1인당 5개로 예매 제한하기 */
+    public void validateReservationSeatLimit(int reservedTicketCount, int seatCount) {
+        if (reservedTicketCount + seatCount > 5) {
+            throw new IllegalArgumentException("상영시간별 예매는 최대 5개까지 할 수 있습니다.");
+        }
+    }
 
+    /* 좌석 중복 여부 검증 */
+    public void validateSeatAvailability(int duplicateReservationCount) {
+        if (duplicateReservationCount > 0) {
+            throw new IllegalArgumentException("선택한 좌석은 이미 예매되었습니다.");
+        }
+    }
+
+    /* 좌석 범위 검증 */
+    public void validateSeatLimit(ScreenSeatLayout screenSeatLayout, List<ScreenSeat> selectedSeats) {
+        for (ScreenSeat seat : selectedSeats) {
+            if (seat.getSeatNumber() > screenSeatLayout.getMaxSeatNumber()) {
+                throw new IllegalArgumentException(
+                        String.format("선택한 좌석 %s은(는) 예매 가능한 좌석 범위를 초과합니다. (최대 %s 좌석까지 가능)",
+                                seat.name(), screenSeatLayout.getSeatRow() + String.format("%02d", screenSeatLayout.getMaxSeatNumber())));
+            }
+        }
+    }
+
+    /* 예매 정보 생성 */
+    public List<TicketReservation> createTicketReservations(ScreeningSchedule screeningSchedule, Member member, ScreenSeat screenSeat, ScreenSeatLayout screenSeatLayout, int seatCount) {
+        //선택좌석부터 연결된 좌석 조회
+        List<ScreenSeat> selectedSeats = ScreenSeat.getSelectedConnectedSeats(screenSeat, seatCount);
+        
+        //좌석 범위 검증
+        validateSeatLimit(screenSeatLayout, selectedSeats);
+
+        // 좌석목록에 전부에 대해 영화예약 도메인 객체 생성하여 리턴
         return selectedSeats.stream()
-                .map(seat -> new TicketReservation(
-                        null,
-                        seat,
-                        screeningSchedule,
-                        member, member.getMemberId(),
-                        null,
-                        null,
-                        null
-                ))
+                .map(seat -> TicketReservation.create(screeningSchedule, member, seat))
                 .collect(Collectors.toList());
     }
 }
