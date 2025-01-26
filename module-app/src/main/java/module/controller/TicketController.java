c27d11af1857db66470b4ff0c74807fc4ed56a7c
유정현 <dbdb1114@naver.com>
2025-01-27 07:14:34 +0900

    [ feat ] app module 티켓 예매 컨트롤러 및 테스트 코드 작성

---
+++ module-app/src/main/java/module/controller/TicketController.java
@@ -0,0 +1,53 @@
+package module.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import dto.ticket.TicketDTO;
+import dto.ticket.TicketReservationRequest;
+import dto.ticket.TicketResponse;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.RequiredArgsConstructor;
+import module.service.ticket.TicketService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/ticket")
+public class TicketController {
+
+	private final TicketService ticketService;
+
+	@GetMapping(value = "/all")
+	public ResponseEntity<List<TicketResponse>> getAllTicket(
+		@NotNull @RequestParam Long showingId
+	) {
+		return ResponseEntity.ok(ticketService.getAllTicket(showingId));
+	}
+
+	@GetMapping(value = "/reserved/userId")
+	public ResponseEntity<List<TicketResponse>> getUserTicket(
+		@Pattern(regexp = "^[a-zA-Z0-9]+$\n", message = "사용자 아이디는 영문및 숫자로 이루어져 있습니다.")
+		@NotNull @RequestParam String username
+	){
+		return ResponseEntity.ok(ticketService.getUserTicket(username));
+	}
+
+	@PostMapping(value = "/reservation")
+	public ResponseEntity<String> reservation(
+		@RequestBody TicketReservationRequest request
+	) {
+		Long showingId = request.getShowingId();
+		String username = request.getUsername();
+		List<TicketDTO> ticketList = request.getTicketList();
+
+		return ResponseEntity.ok(ticketService.reservation(showingId,username,ticketList));
+	}
+}
