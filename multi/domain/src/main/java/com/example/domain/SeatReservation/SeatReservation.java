97ecc6ff53ed401aea0a7dfbcc95682bda8748a5
Taehwan2 <97010824+Taehwan2@users.noreply.github.com>
2025-01-27 20:45:44 +0900

    API 구현 완료

--- multi/domain/src/main/java/com/example/domain/SeatReservation/SeatReservation.java
+++ multi/domain/src/main/java/com/example/domain/SeatReservation/SeatReservation.java
@@ -1,22 +1,30 @@
-package com.example.domain.SeatReservation;
+package com.example.domain.seatReservation.entity;
 
-import com.example.domain.common.BaseEntity;
-import com.example.domain.screening.Screening;
+import com.example.domain.common.entity.BaseEntity;
+import com.example.domain.screening.entity.Screening;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "SeatReservations")
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Setter
 public class SeatReservation extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "seat_reservation_id")
     private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
 
     @ManyToOne
     @JoinColumn(name = "screening_id")
@@ -28,5 +36,21 @@ public class SeatReservation extends BaseEntity {
     @Column(name = "is_reserved")
     private boolean isReserved;
 
+
+
+    public static List<SeatReservation> makeReservation(int count, List<String> seats, Screening screening, Long userId){
+        List<SeatReservation> reservations = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            reservations.add(
+                    SeatReservation.builder()
+                            .userId(userId)
+                            .seatNumber(seats.get(i))
+                            .screening(screening)
+                            .isReserved(true)
+                            .build()
+            );
+        }
+        return reservations;
+    }
 
 }
--- multi/domain/src/main/java/com/example/domain/SeatReservation/service/SeatReservationRepository.java
+++ multi/domain/src/main/java/com/example/domain/SeatReservation/service/SeatReservationRepository.java
@@ -1,10 +1,15 @@
-package com.example.domain.SeatReservation.service;
+package com.example.domain.seatReservation.service;
 
-import com.example.domain.SeatReservation.SeatReservation;
+import com.example.domain.seatReservation.entity.SeatReservation;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SeatReservationRepository {
     public SeatReservation getSeatReservationInfo(Long seatReservationId);
+    public List<SeatReservation> getSeatReservationInfoByUser(Long userId);
+    Boolean checkReserveAble(List<String> seats);
 
+    void saveSeats(List<SeatReservation> seatReservations);
 }
