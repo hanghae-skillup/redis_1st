7d1be8ac421d1fdee976ebc3f4fa1a914e1f46d6
정소민 <yjk70702002@gmail.com>
2025-01-30 02:26:36 +0900

    refactor: 필요없는 쿼리 제거

--- module-domain/src/main/java/org/example/repository/ReservationJpaRepository.java
+++ module-domain/src/main/java/org/example/repository/ReservationJpaRepository.java
@@ -1,14 +1,7 @@
 package org.example.repository;
 
 import org.example.domain.reservation.Reservation;
-import org.example.dto.SeatsDto;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface ReservationJpaRepository extends JpaRepository<Reservation, Long> {
-    @Query("select r.id from Reservation r where r.screenScheduleId=:screenScheduleId")
-    Long findIdByScreenScheduleId(@Param("screenScheduleId") Long screenScheduleId);
 }
