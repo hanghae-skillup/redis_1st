0e2d963c54fb60a7db8570d5f3502149bdb709a9
futuremaker <futuremaker019@gmail.com>
2025-01-31 23:23:23 +0900

    [ feature ] ReservationManager/AOP 분산락과 함수형 기반 분산락을 분리하여 구현함

--- movie-domain/domain/src/main/java/com/movie/domain/facade/ReservationManager.java
+++ movie-domain/domain/src/main/java/com/movie/domain/facade/ReservationManager.java
@@ -15,17 +15,23 @@ public class ReservationManager {
     private final ReservationFacade reservationFacade;
     private final DistributedLockExecutor executor;
 
+    /**
+        AOP 형태의 분산락 적용
+     */
     @DistributedLock(lockName = "reservation")
     public void makeReservationByDistributedLock(String token, Long scheduleId, List<Long> seatIds) {
         ReservationCommand.GetReserveData reserveData = ReservationCommand.GetReserveData.of(scheduleId, seatIds, token);
-        reservationFacade.makeReservation(reserveData);
+        reservationFacade.makeReservations(reserveData);
     }
 
+    /**
+        함수형 분산락 적용
+     */
     public void makeReservationByFunctionalLock(String token, Long scheduleId, List<Long> seatIds) {
         String lockName = "%s:%s:%s".formatted("reservation", System.currentTimeMillis(), token);
         executor.executeWithLock(lockName, () -> {
             ReservationCommand.GetReserveData reserveData = ReservationCommand.GetReserveData.of(scheduleId, seatIds, token);
-            reservationFacade.makeReservation(reserveData);
+            reservationFacade.makeReservations(reserveData);
         });
     }
 
--- movie-infrastructures/storage/src/test/java/com/movie/storage/facade/ReservationFacadeTest.java
+++ movie-infrastructures/storage/src/test/java/com/movie/storage/facade/ReservationFacadeTest.java
@@ -1,6 +1,7 @@
 package com.movie.storage.facade;
 
-import com.movie.domain.facade.ReservationManager;
+import com.movie.domain.facade.ReservationFacade;
+import com.movie.domain.movie.dto.command.ReservationCommand;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +17,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ReservationFacadeTest {
 
     @Autowired
-    private ReservationManager reservationManager;
+    private ReservationFacade reservationFacade;
 
-    @DisplayName("AOP 분산락을 적용하여, 예약 기능에 동시성 발생 유무를 확인")
+    @DisplayName("동시성이 발생되는 ")
     @Test
-    void shouldHandleConcurrentReservationsByAOPDistributedLock() throws InterruptedException {
+    void test() throws InterruptedException {
         // given
         int threadCount = 100;
         ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
@@ -39,22 +40,22 @@ class ReservationFacadeTest {
         // when
         for (int i = 0; i < threadCount; i++) {
             executorService.submit(() -> {
-               try {
-                   String token = tokens.get(ThreadLocalRandom.current().nextInt(tokens.size()));
-                   reservationManager.makeReservationByDistributedLock(token,scheduleId,seatIds);
-                   count.getAndIncrement();
-               } catch (Exception e) {
-                   e.printStackTrace();
-               } finally {
-                   latch.countDown();
-               }
+                try {
+                    String token = tokens.get(ThreadLocalRandom.current().nextInt(tokens.size()));
+                    reservationFacade.makeReservationsByFunctionalLock(ReservationCommand.GetReserveData.of(scheduleId, seatIds, token));
+                    count.getAndIncrement();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    latch.countDown();
+                }
             });
         }
 
         executorService.shutdown();
         executorService.awaitTermination(1, TimeUnit.MINUTES);
 
-        // 자리예약은 한번만 성공하도록 함
+        // then  - 자리예약은 한번만 성공하도록 함
         assertThat(count.get()).isEqualTo(1);
     }
 
