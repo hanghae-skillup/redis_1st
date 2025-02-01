366cbb54fe68841bc32466dc1e965fdd6eb1fb5a
futuremaker <futuremaker019@gmail.com>
2025-02-01 22:56:18 +0900

    [ feature ] application 모듈 추가로 인한 빌드 구성 변경

--- movie-infrastructures/storage/src/test/java/com/movie/storage/MovieStorageApplicationTests.java
+++ movie-infrastructures/storage/src/test/java/com/movie/storage/MovieStorageApplicationTests.java
@@ -2,11 +2,9 @@ package com.movie.storage;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = {
-		"com.movie.domain", "com.movie.storage", "com.movie.redis"
+		"com.movie.domain", "com.movie.storage", "com.movie.redis",
 })
 class MovieStorageApplicationTests {
 
