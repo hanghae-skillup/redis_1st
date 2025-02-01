56b5943d6a44463117e4aedbadb28212cc1d752a
Jaemin Park <papk0904@naver.com>
2025-02-02 01:06:26 +0900

    add RateLimit to API using guava

--- app/src/main/java/com/movie/app/controller/MovieRestController.java
+++ app/src/main/java/com/movie/app/controller/MovieRestController.java
@@ -1,6 +1,10 @@
 package com.movie.app.controller;
 import java.util.List;
 
+import com.google.common.util.concurrent.RateLimiter;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.movie.app.domain.Movie;
@@ -8,6 +12,7 @@ import com.movie.app.domain.MovieRequestDto;
 import com.movie.app.domain.TicketingRequestDto;
 import com.movie.app.service.MovieService;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 
@@ -17,19 +22,31 @@ import lombok.RequiredArgsConstructor;
 public class MovieRestController {
 
     private final MovieService movieService;
+    private static RateLimiter moviesRateLimiter;
+    private static RateLimiter ticketingLimiter;
+
+    @PostConstruct
+    public void init() {
+        moviesRateLimiter = RateLimiter.create(0.8);//50permits/60sec = 0.8permits/1sec
+        ticketingLimiter = RateLimiter.create(0.003);//1permits/5min = 0.003permits/1sec
+    }
 
     @GetMapping("/api/movies")
-    public List<Movie> getMovies(
+    public ResponseEntity<List<Movie>> getMovies(
         @Size(max = 100, message = "title length should not exceed 100 characters")
         @RequestParam(required=false) String title,
         @RequestParam(required=false) String genre) {
+
+            if(!moviesRateLimiter.tryAcquire()) {
+                return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+            }
         
             if(genre != null) {
-                return movieService.getMoviesByGenre(genre);
+                return ResponseEntity.ok(movieService.getMoviesByGenre(genre));
             } else if (title != null) {
-                return movieService.getMoviesByTitle(title);
+                return ResponseEntity.ok(movieService.getMoviesByTitle(title));
             } else {
-                return movieService.getMoviesAll();
+                return ResponseEntity.ok(movieService.getMoviesAll());
             }     
     }
 
@@ -39,7 +56,11 @@ public class MovieRestController {
     }
 
     @PutMapping("/api/ticketing/{id}")
-    public Movie ticketingMovie(@PathVariable Long id, @RequestBody TicketingRequestDto requestDto) {
-        return movieService.ticketing(id ,requestDto);
+    public ResponseEntity<Movie> ticketingMovie(@PathVariable Long id, @RequestBody TicketingRequestDto requestDto) {
+        if(!ticketingLimiter.tryAcquire()) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
+
+        return ResponseEntity.ok(movieService.ticketing(id ,requestDto));
     }
 }
