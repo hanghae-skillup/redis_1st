90d5c3b9b6790cf5faaf7030abc6c43c512ffd16
유정현 <dbdb1114@naver.com>
2025-02-06 16:21:14 +0900

    [ refactor ] service module lock 과정 메소드 분리

--- module-service/src/main/java/module/lock/aop/DistributedLockAop.java
+++ module-service/src/main/java/module/lock/aop/DistributedLockAop.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -29,7 +30,7 @@ public class DistributedLockAop {
 
 	@Around("@annotation(module.lock.aop.DistributedLock)")
 	public Object lock(final ProceedingJoinPoint joinPoint) throws Throwable {
-		MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+		MethodSignature signature = (MethodSignature)joinPoint.getSignature();
 		Method method = signature.getMethod();
 		DistributedLock distributedLock = method.getAnnotation(DistributedLock.class);
 
@@ -39,14 +40,7 @@ public class DistributedLockAop {
 
 		// Lock 대상이 여러 개일 경우를 대비하여
 		// Collection이나 Array로 변환하여 Lock 점유
-		Object[] keysArr;
-		if(keys instanceof Collection<?>){
-			keysArr = ((Collection<?>)keys).toArray();
-		} else if( keys.getClass().isArray() ){
-			keysArr = (Object[]) keys;
-		} else {
-			keysArr = new Object[] {keys};
-		}
+		Object[] keysArr = convertToArray(keys);
 
 		// key 배열만큼 락 생성
 		List<RLock> lockList = Arrays.stream(keysArr)
@@ -54,45 +48,58 @@ public class DistributedLockAop {
 			.map(key -> redissonClient.getLock(REDISSON_LOCK_PREFIX + keyPrefix + key))
 			.toList();
 
-		try{
+		try {
 			// 락 점유 하나라도 점유 실패 시 TryLockFailedException
-			boolean allAvailable = true;
-			for (RLock lock : lockList) {
-				try{
-					if(!lock.tryLock(distributedLock.waitTime(), distributedLock.leaseTime(),distributedLock.timeUnit())){
-						allAvailable = false;
-						break;
-					}
-				} catch (InterruptedException e){
-					allAvailable = false;
-					break;
-				}
-			}
+			boolean allAvailable = acquireLock(lockList, distributedLock.waitTime(), distributedLock.leaseTime(),
+				distributedLock.timeUnit());
 
-			// 배열내 모든 락이 점유 실패
-			if(!allAvailable){
-				// 순차적으로 락을 해제하도록 변경
-				for (RLock lock : lockList) {
-					if (lock.isHeldByCurrentThread()) {
-						lock.unlock();
-					}
-				}
+			// 배열내 모든 락이 점유 실패시
+			// 점유한 락 해제
+			if (!allAvailable) {
+				leaseAllLock(lockList);
 				throw new TryLockFailedException();
 			}
 
 			return aopForTransaction.proceed(joinPoint);
 		} finally {
 			// 메소드 종료 후 모든 락 점유 해제
-			lockList.forEach(lock -> {
-				if (lock.isHeldByCurrentThread()) {
-					try {
-						lock.unlock();
-					} catch (IllegalMonitorStateException e) {
-						log.warn("Lock was already released: {}", lock.getName());
-					}
-				}
-			});
+			leaseAllLock(lockList);
 		}
 
+	}
+
+	private void leaseAllLock(List<RLock> lockList) {
+		for (RLock lock : lockList) {
+			if (lock.isHeldByCurrentThread()) {
+				try {
+					lock.unlock();
+				} catch (IllegalMonitorStateException e) {
+					log.warn("Lock was already released: {}", lock.getName());
+				}
+			}
+		}
+	}
+
+	private boolean acquireLock(List<RLock> lockList, Long waitTIme, Long leaseTime, TimeUnit timeUnit) {
+		for (RLock lock : lockList) {
+			try {
+				if (!lock.tryLock(waitTIme, leaseTime, timeUnit)) {
+					return false;
+				}
+			} catch (InterruptedException e) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private Object[] convertToArray(Object keys) {
+		if (keys instanceof Collection<?>) {
+			return ((Collection<?>)keys).toArray();
+		} else if (keys.getClass().isArray()) {
+			return (Object[])keys;
+		} else {
+			return new Object[] {keys};
+		}
 	}
 }
