90d80a6f8a0fb2f9d2d4645639dd11ba015d480f
유정현 <dbdb1114@naver.com>
2025-02-09 18:00:55 +0900

    [ feat ] app module redis Rate Limiter 구현

--- module-app/src/main/java/module/config/ratelimit/limiters/ShowingSearchRateLimiter.java
+++ module-app/src/main/java/module/config/ratelimit/limiters/ShowingSearchRateLimiter.java
@@ -1,89 +1,72 @@
 package module.config.ratelimit.limiters;
 
-import java.util.concurrent.TimeUnit;
+import java.util.Collections;
 
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.util.concurrent.RateLimiter;
-
 import exception.showing.TooManyRequestException;
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 public class ShowingSearchRateLimiter implements module.config.ratelimit.limiters.RateLimiter {
 
-	private static final double TOKEN_PER_SECOND = 2;
-	private static final int LIMIT_PER_MINUTE = 50;
+	private static final String LIMIT_PER_SECOND = "2";
+	private static final String LIMIT_PER_MINUTE = "50";
+	private static final DefaultRedisScript<Boolean> LUA_SCRIPT = new DefaultRedisScript(
+		"""
+			local prefix = KEYS[1]
+			local ip = ARGV[1]
+			local LIMIT_PER_SECOND = tonumber(ARGV[2])
+			local LIMIT_PER_MINUTE = tonumber(ARGV[3])
+			
+			local blocked_ip_key = prefix .. ':BlockedIp:' .. ip
+			local request_counter_key = prefix .. ':PerMinuteRequestCounter:' .. ip
+			local per_second_counter_key = prefix .. ':PerSecondRequestCounter:' .. ip
+			
+			if redis.call('EXISTS', blocked_ip_key) == 1 then 
+				return false 
+			end
+			
+			local per_second_count = redis.call('INCR', per_second_counter_key)
+			if per_second_count == 1 then 
+				redis.call('EXPIRE', per_second_counter_key, 1) end
+			if per_second_count > LIMIT_PER_SECOND then
+				return false 
+			end
+			
+			local current_count = redis.call('INCR', request_counter_key)
+			if current_count == 1 then 
+				redis.call('EXPIRE', request_counter_key, 60) 
+			end
+			if current_count > LIMIT_PER_MINUTE then
+			   redis.call('SET', blocked_ip_key, 1)
+			   redis.call('EXPIRE', blocked_ip_key, 3600)
+			   return false
+			end
+			
+			return true
+		""", Boolean.class);
 
-	private final Cache<String, Boolean> blockedIp = CacheBuilder.newBuilder()
-		.expireAfterWrite(1, TimeUnit.HOURS)
-		.build();
-
-	private final Cache<String, Integer> requestCount = CacheBuilder.newBuilder()
-		.expireAfterWrite(1, TimeUnit.MINUTES)
-		.build();
-
-	private final Cache<String, com.google.common.util.concurrent.RateLimiter> ipRateLimiter = CacheBuilder.newBuilder()
-		.expireAfterAccess(1, TimeUnit.MINUTES)
-		.build();
+	private final StringRedisTemplate redisTemplate;
+	private final String KEY_PREFIX = "hanghaeho:showingSearch";
 
 	@Override
 	public boolean preCheck(Object[] args, ContentCachingRequestWrapper request){
 		String ip = request.getRemoteAddr();
-		increaseRequestCount(ip);
-		return isAllowed(ip);
-	}
+		boolean isAllowed = Boolean.TRUE.equals(redisTemplate.execute(
+			LUA_SCRIPT,
+			Collections.singletonList(KEY_PREFIX),
+			ip, LIMIT_PER_SECOND, LIMIT_PER_MINUTE
+		));
 
-	public boolean isAllowed(String ip){
-		int count = requestCount.getIfPresent(ip);
-		System.out.println(count + "회 요청");
-
-		if(count > LIMIT_PER_MINUTE){
-			addBlock(ip);
-			throw new TooManyRequestException();
-		}
-
-		if(isBlocked(ip)){
-			throw new TooManyRequestException();
-		}
-
-		RateLimiter rateLimiter = getRateLimiter(ip);
-		if(!rateLimiter.tryAcquire()){
+		if(!isAllowed){
 			throw new TooManyRequestException();
 		}
 
 		return true;
-	}
-
-	public RateLimiter getRateLimiter(String ip){
-		RateLimiter rateLimiter = ipRateLimiter.getIfPresent(ip);
-
-		if(rateLimiter == null){
-			rateLimiter = com.google.common.util.concurrent.RateLimiter.create(TOKEN_PER_SECOND);
-			ipRateLimiter.put(ip,rateLimiter);
-		}
-
-		return rateLimiter;
-	}
-
-	public void increaseRequestCount(String ip){
-		Integer count = requestCount.getIfPresent(ip);
-
-		if(count == null) {
-			count = 0;
-		}
-
-		// count 증가
-		requestCount.put(ip, ++count);
-	}
-
-	public boolean isBlocked(String ip) {
-		return blockedIp.getIfPresent(ip) != null;
-	}
-
-	public void addBlock(String ip) {
-		blockedIp.put(ip, true);
 	}
 }
--- module-app/src/main/java/module/config/ratelimit/limiters/TicketReservationRateLimiter.java
+++ module-app/src/main/java/module/config/ratelimit/limiters/TicketReservationRateLimiter.java
@@ -1,14 +1,10 @@
 package module.config.ratelimit.limiters;
 
-import java.util.concurrent.TimeUnit;
-
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
-
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
 
 import dto.ticket.TicketReservationRequest;
 import exception.ticket.NoReservationInfoException;
@@ -19,14 +15,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class TicketReservationRateLimiter implements RateLimiter {
 
-	private final Cache<String, Boolean> blockedUser = CacheBuilder.newBuilder()
-		.expireAfterWrite(5, TimeUnit.MINUTES)
-		.build();
+	private final String KEY_PREFIX = "hanghaeho:ticket_reservation_complete:";
+	private final RedisTemplate<String, String> redisTemplate;
 
 	@Override
 	public boolean preCheck(Object[] args, ContentCachingRequestWrapper request) {
 		TicketReservationRequest ticketReservationRequest = getTicketReservationRequest(args);
-
 		Long showingId = ticketReservationRequest.getShowingId();
 		String username = ticketReservationRequest.getUsername();
 
@@ -52,12 +46,13 @@ public class TicketReservationRateLimiter implements RateLimiter {
 	}
 
 	public boolean isBlocked(String user, Long showingId) {
-		return blockedUser.getIfPresent(blockKeyFormatter(user, showingId)) != null;
+		String key = keyFormatter(user, showingId);
+		return redisTemplate.opsForValue().get(key) != null;
 	}
 
 	public void addBlock(String username, Long showingId) {
-		String key = blockKeyFormatter(username, showingId);
-		blockedUser.put(key, true);
+		String key = keyFormatter(username, showingId);
+		redisTemplate.opsForValue().set(key, "complete", 300000);
 	}
 
 	public TicketReservationRequest getTicketReservationRequest(Object[] args){
@@ -69,7 +64,7 @@ public class TicketReservationRateLimiter implements RateLimiter {
 		throw new NoReservationInfoException();
 	}
 
-	public String blockKeyFormatter(String username, Long showingId){
-		return username + "|" + showingId;
+	public String keyFormatter(String username, Long showingId){
+		return KEY_PREFIX + username + ":" + showingId;
 	}
 }
